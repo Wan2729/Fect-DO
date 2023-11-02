da22037d1f96baf5eb74d18ffd69d684a61d4fac
Wan2729 <wanharithazdy12@gmail.com>
2023-11-02 14:59:21 +0800

    TODO (Sign Up Page)

--- app/src/main/java/com/example/fectdo/MainActivity.java
+++ app/src/main/java/com/example/fectdo/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.fectdo;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signUpButton = (ImageView)findViewById(R.id.signUpButton);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSignUpPage();
+            }
+        });
+    }
+
+    public void goToSignUpPage(){
+        Intent intent = new Intent(this,SignUp.class);
+        MainActivity.this.startActivity(intent);
     }
 }
