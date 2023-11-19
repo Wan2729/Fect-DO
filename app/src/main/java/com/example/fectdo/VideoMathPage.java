058fd1275fe6624e016e7310df789e27cac324af
azim <u2102134@siswa.um.edu.my>
2023-11-19 14:54:33 +0800

    Math page done 20%

---
+++ app/src/main/java/com/example/fectdo/VideoMathPage.java
@@ -0,0 +1,14 @@
+package com.example.fectdo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VideoMathPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_math_page);
+    }
+}
