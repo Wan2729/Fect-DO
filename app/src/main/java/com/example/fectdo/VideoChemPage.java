ee31293c53e1e02b11eff25f25e4d61e04315233
Abdul Hanif <hanifaziz652@gmail.com>
2023-12-04 19:55:11 +0800

    Chemistry Video Page

---
+++ app/src/main/java/com/example/fectdo/VideoChemPage.java
@@ -0,0 +1,14 @@
+package com.example.fectdo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VideoChemPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_chem_page);
+    }
+}
