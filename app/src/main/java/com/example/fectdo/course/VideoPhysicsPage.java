021cdcac1cd4e7caed4dcfc2bab5e234eead6663
Razin <razinnazarudin@gmail.com>
2023-12-10 01:06:45 +0800

    Razin - toolbar and bottom fragment physics

--- app/src/main/java/com/example/fectdo/course/VideoPhysicsPage.java
+++ app/src/main/java/com/example/fectdo/course/VideoPhysicsPage.java
@@ -1,13 +1,20 @@
 package com.example.fectdo.course;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.fectdo.R;
+import com.example.fectdo.general.LoginEmailPassword;
+import com.example.fectdo.utils.FirebaseUtil;
 
 public class VideoPhysicsPage extends AppCompatActivity {
 
@@ -15,6 +22,8 @@ public class VideoPhysicsPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.video_physics_page);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
 
 
@@ -29,5 +38,28 @@ public class VideoPhysicsPage extends AppCompatActivity {
     void LetsgoExam(){
         Intent ExamPhy=new Intent(this, ExamMainPage.class);
         startActivity(ExamPhy);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logoutBtn:
+                handleLogout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void handleLogout() {
+        FirebaseUtil.logOut();
+        Intent intent = new Intent(VideoPhysicsPage.this, Enroll.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
